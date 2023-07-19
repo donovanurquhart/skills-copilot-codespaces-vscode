67b974e065eba5c25ec63ee60c5346f3310d2113
donovanurquhart <139967632+donovanurquhart@users.noreply.github.com>
2023-07-19 11:29:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var project = document.getElementById("project");
+  var contact = document.getElementById("contact");
+  var about = document.getElementById("about");
