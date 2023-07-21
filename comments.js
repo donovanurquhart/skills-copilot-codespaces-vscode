fc0d83b73ef0a965864ad58db6952db0c8941164
donovanurquhart <139967632+donovanurquhart@users.noreply.github.com>
2023-07-21 13:46:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,51 @@
 // create web server
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-
-// create express app
 const app = express();
+const port = 3000;
 
-// middleware
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+// connect to database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/express-demo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
-// get all comments
-app.get('/posts/:id/comments', (req, res) => {
-    res.send([]);
+// create schema
+const commentSchema = new mongoose.Schema({
+    name: String,
+    comment: String,
+    date: { type: Date, default: Date.now }
 });
 
-// create a comment
-app.post('/posts/:id/comments', (req, res) => {
-    res.send({});
+// create model
+const Comment = mongoose.model('Comment', commentSchema);
+
+// set view engine
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// use static files
+app.use(express.static('public'));
+
+// use middleware
+app.use(express.urlencoded({ extended: true }));
+
+// get request
+app.get('/', (req, res) => {
+    Comment.find().then(comments => {
+        res.render('index', { comments: comments });
+    });
 });
 
-// start server
-app.listen(4001, () => {
-    console.log('Listening on 4001');
-});    
+// post request
+app.post('/', (req, res) => {
+    const comment = new Comment({
+        name: req.body.name,
+        comment: req.body.comment
+    });
+    comment.save().then(() => {
+        res.redirect('/');
+    });
+});
+
+// listen to port
+app.listen(port, () => console.log(`Listening on port ${port}...`));
