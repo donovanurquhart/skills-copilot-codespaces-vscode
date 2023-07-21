2fa48e722ff17ce26ae18d8fbc74bec3d5b8ee83
donovanurquhart <139967632+donovanurquhart@users.noreply.github.com>
2023-07-21 11:29:05 +0000

    Copliot second commit

--- member.js
+++ member.js
@@ -1,6 +1,13 @@
-function skillsMember () {
-  return {
-    restrict: 'E',
-    templateUrl: 'views/member/skills.html'
-  }
+function skillsmember() {
+    var skills = document.getElementById("skills").value;
+    var skillserror = document.getElementById("skillserror");
+
+    if (skills == "") {
+        skillserror.innerHTML = "Please enter your skills";
+        return false;
+    }
+    else {
+        skillserror.innerHTML = "";
+        return true;
+    }
 }
